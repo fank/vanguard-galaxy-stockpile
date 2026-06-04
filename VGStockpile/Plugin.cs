925f5f45e26e81ab973f3b07f6c92c9fb19c4fab
Florian Kinder <florian.kinder@enthus.de>
2026-06-04 20:31:19 +0200

    chore: bump PluginVersion to 0.4.0

--- VGStockpile/Plugin.cs
+++ VGStockpile/Plugin.cs
@@ -25,7 +25,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PluginGuid    = "vgstockpile";
     public const string PluginName    = "Vanguard Galaxy Stockpile";
-    public const string PluginVersion = "0.3.0";
+    public const string PluginVersion = "0.4.0";
 
     internal static Plugin          Instance { get; private set; } = null!;
     internal static ManualLogSource Log      { get; private set; } = null!;
